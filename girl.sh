2ec402fcaf5a8776c8894466d398064c367dfdbb
wxn0brP <toja.filiper@gmail.com>
2025-11-11 12:36:14 +0100

    feat: update

---
+++ girl.sh
@@ -0,0 +1,6 @@
+cd native
+./girl.sh
+cd ..
+bun install
+bun run build
+bun run src/index.ts
--- src/front/index.ts
+++ src/front/index.ts
@@ -13,24 +13,15 @@ function _fetch(url: string) {
 const commands: CommandMap = {
     start: [
         { name: "exit", action: () => { _fetch("exit").then(window.close) } },
-        { name: "hide", action: () => { _fetch("hide") } },
-        { name: "empty", action: () => { } },
-        { name: "empty", action: () => { } },
-        { name: "empty", action: () => { } },
         { name: "empty", action: () => { } },
         { name: "empty", action: () => { } },
         { name: "empty", action: () => { } },
     ]
 }
 
-const menu = new WolfMenu(
-    commands,
-    document.querySelector(".wolf")
-);
-menu._cancelCommand = {
-    name: "cancel",
-    action: () => { _fetch("hide") }
-};
+const wolf = document.querySelector<HTMLDivElement>(".wolf");
+const menu = new WolfMenu(commands, wolf);
+
 menu._logFn = (...any: any[]) => {
     console.error("front", ...any);
 };
@@ -40,6 +31,23 @@ menu.init();
 (window as any)._wolf = (x: string, y: string) => {
     menu._x = +x;
     menu._y = +y;
-    console.error(menu._x, menu._y);
-    menu._open("start");
+    console.error("menu", menu._x, menu._y);
+    document.addEventListener("mousemove", () => {
+        menu._open("start");
+        menu.distanceAccept = true;
+    }, { once: true });
 }
+
+menu.emitter.on("menuClosed", () => {
+    _fetch("hide");
+});
+
+menu.emitter.on("menuOpened", () => {
+    menu.distanceAccept = false;
+});
+
+menu.emitter.on("distance", (distance: number) => {
+    const maxDistance = wolf.clientWidth - 10;
+    const percent = Math.min(1, distance / maxDistance);
+    wolf.style.setProperty("--alpha", percent.toString());
+});
